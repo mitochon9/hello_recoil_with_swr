19cbbf1cba90d0f51795d96cd8003caea3839682
mitochon9 <mitochon9@gmail.com>
2022-05-22 01:05:02 +0900

    アイコン実装

---
+++ src/component/atom/Icon/Icon.tsx
@@ -0,0 +1,19 @@
+import { PencilAltIcon, TrashIcon, CheckCircleIcon, XCircleIcon } from '@heroicons/react/solid';
+import { IconProps } from './Icon.type';
+
+export const Icon: React.FC<IconProps> = ({ type }) => {
+  switch (type) {
+    case 'edit':
+      return <PencilAltIcon className='w-8 text-primary-600 duration-300 hover:text-primary-700' />;
+    case 'trash':
+      return <TrashIcon className='w-8 text-primary-600 duration-300 hover:text-primary-700' />;
+    case 'check':
+      return (
+        <CheckCircleIcon className='w-8 text-primary-600 duration-300 hover:text-primary-700' />
+      );
+    case 'cancel':
+      return <XCircleIcon className='w-8 text-primary-600 duration-300 hover:text-primary-700' />;
+    default:
+      return null;
+  }
+};
---
+++ src/component/atom/Icon/Icon.type.ts
@@ -0,0 +1,4 @@
+export interface IconProps {
+  children?: React.ReactNode;
+  type: 'edit' | 'trash' | 'check' | 'cancel';
+}

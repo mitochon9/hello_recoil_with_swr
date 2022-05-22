301d9c93da3dbc01b093160e9f2208603cb4333b
mitochon9 <mitochon9@gmail.com>
2022-05-22 18:41:24 +0900

    見た目部分の変更

--- src/component/hello_recoil_with_swr/TodoList/TodoList.tsx
+++ src/component/hello_recoil_with_swr/TodoList/TodoList.tsx
@@ -5,10 +5,13 @@ export const baseId = 'project-todo-list';
 
 export const TodoList: React.FC<TodoListProps> = ({ todoList }) => (
   <>
-    <div className='grid gap-y-4'>
-      {todoList.map((todo) => (
-        <TodoItem key={todo.todo.id} {...todo} />
-      ))}
+    <div>
+      <h2>Todo List</h2>
+      <div className='grid gap-y-4 rounded-lg border-2 border-primary-600 bg-primary-100 p-4'>
+        {todoList?.map((todo) => (
+          <TodoItem key={todo.todo.id} {...todo} />
+        ))}
+      </div>
     </div>
   </>
 );
